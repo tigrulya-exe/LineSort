7e277682993bfb14d17c4584b7c72fb5edba4fb2
Tigran Manasyan <manasyan_tigran@mail.ru>
2019-07-18 04:24:26 +0700

    + Help option information was added

--- src/main/java/nsu/manasyan/linesort/arguments/CommandLineOptionsData.java
+++ src/main/java/nsu/manasyan/linesort/arguments/CommandLineOptionsData.java
@@ -4,28 +4,33 @@ import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionsBase;
 
 public class CommandLineOptionsData extends OptionsBase {
+    @Option(name = " *without key*",
+            category = "required",
+            help = "Path to directory of files to be sorted. Must be first option.",
+            defaultValue = "")
+    public String directoryPath;
 
     @Option(name = "out-prefix",
-            help = "Prefix, which'll be added to the beginning of the sorted files",
+            help = "Prefix, which'll be added to the beginning of the sorted files.",
             category = "required",
             defaultValue = "")
     public String outPrefix;
 
     @Option(name = "content-type",
-            help = "Type of file content",
+            help = "Type of file content.",
             category = "required",
             defaultValue = "")
     public String contentType;
 
     @Option(name = "sort-mode",
             category = "required",
-            help = "Ascending or descending order",
+            help = "Ascending or descending order.",
             defaultValue = "")
     public String sortMode;
 
     @Option(name = "help",
             category = "optional",
-            help = "Show information about all options\n\nExecution template: java -jar *jar-file path* [path to directory of files to be sorted] [other options]",
+            help = "Show information about all options.\n\nExecution template: java -jar *jar-file path* [path to directory of files to be sorted] [other options]",
             defaultValue = "")
     public String help;
 }
